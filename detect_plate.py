45e21f4404ef3a8220ba9b9335d519948c017444
Timothy Chang <tyc2118@columbia.edu>
2025-04-30 12:41:05 -0400

    detect_plate.py working

--- detect_plate.py
+++ detect_plate.py
@@ -4,33 +4,34 @@ from ultralytics import YOLO
 
 # Load image and model
 image_path = "test_image.jpg"
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("best.pt")
 img = cv2.imread(image_path)
+print("HELLOOO")
 
 if img is None:
-	raise ValueError("Image could not be loaded.")
+    raise ValueError("Image could not be loaded.")
 
 # Detect plates
 results = model(image_path)[0]
 
 for i, box in enumerate(results.boxes):
-	x1, y1, x2, y2 = map(int, box.xyxy[0])
-	plate_crop = img[y1:y2, x1:x2]
+    x1, y1, x2, y2 = map(int, box.xyxy[0])
+    plate_crop = img[y1:y2, x1:x2]
 
-	# OCR preprocessing
-	gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
-	resized = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-	_, thresh = cv2.threshold(resized, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # OCR preprocessing
+    gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    _, thresh = cv2.threshold(resized, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-	# clean noise
-	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-	cleaned = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # clean noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    cleaned = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-	# Run OCR
-	text = pytesseract.image_to_string(cleaned, config='--psm 7')
-	text = text.strip()
+    # Run OCR
+    text = pytesseract.image_to_string(cleaned, config='--psm 7')
+    text = text.strip()
 
-	print(f"[Plate {i+1}] OCR Result: '{text}'")
+    print(f"[Plate {i+1}] OCR Result: '{text}'")
 
-	if len(text) < 4:
-    	print("Plate may be obstructed or unreadable.")
+    if len(text) < 4:
+        print("Plate may be obstructed or unreadable.")
